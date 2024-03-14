2c3ccc88f2ebeea20272f3f0ad237f6a36b58457
TabithaMusyoka <tabithamusyoka.2000@gmail.com>
2024-03-14 16:51:36 +0300

    changes t

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: "http://192.168.88.164:7700",
-  API: "http://192.168.88.164:7700"
+  baseUrl: "http://192.168.89.109:7700",
+  API: "http://192.168.89.109:7700"
   };
 //   baseUrl: "http://192.168.100.17:8082",
 //   API: "http://192.168.100.17:8082"
