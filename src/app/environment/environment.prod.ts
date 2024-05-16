d48fad3b74a8b2fa2e782ea1536368e1ad99c402
kashingi <moriskashing74@gmail.com>
2024-05-16 11:21:34 +0300

    replenish teller button

--- src/app/environment/environment.prod.ts
+++ src/app/environment/environment.prod.ts
@@ -1,7 +1,10 @@
 export const environment = {
     production: true,
-    baseUrl: "http://192.168.90.52:7700",
-    API: "http://192.168.90.52:7700"
+    // baseUrl: "http://192.168.90.52:7700",
+    // API: "http://192.168.90.52:7700"
     // baseUrl: "http://192.168.100.17:8082",
     // API: "http://192.168.100.17:8082"
+    baseUrl: "localhost:7700",
+    API: "localhost:7700"
+    
   };
