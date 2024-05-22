9b8647fdb3072c9031119e07fe0b17eb8f814f8a
TabithaMusyoka <tabithamusyoka.2000@gmail.com>
2024-05-22 14:16:42 +0300

    changes

--- src/app/back-office/back-office-routing.module.ts
+++ src/app/back-office/back-office-routing.module.ts
@@ -5,13 +5,16 @@ import { AddTellerComponent } from './add-teller/add-teller.component';
 import { ReplenishComponent } from './replenish/replenish.component';
 import {MatInputModule} from '@angular/material/input';
 import { ViewGlsComponent } from '../tabs/view-gls/view-gls.component';
+import { AllReferralsComponent } from '../tabs/all-referrals/all-referrals.component';
 
 const routes: Routes = [
   {path:'', component: ReferralsComponent},
   {path:'referrals', component: ReferralsComponent},
   {path:'add-teller', component: AddTellerComponent},
   {path:'replenish', component: ReplenishComponent},
-  {path:'view-gls', component: ViewGlsComponent}
+  {path:'view-gls', component: ViewGlsComponent},
+  {path:'all-referrals', component: AllReferralsComponent}
+
 
 ];
 
